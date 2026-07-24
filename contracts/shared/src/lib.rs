444e1f726f59ad4c0f8519bb21d0c58fa871510b
Swayymalcolm99 <alexahmed4192@gmail.com>
2026-07-24 08:37:16 +0100

    updated files
    Changes made

--- contracts/shared/src/lib.rs
+++ contracts/shared/src/lib.rs
@@ -2,6 +2,10 @@
 
 pub mod reentrancy_guard;
 pub mod state_machine;
+pub mod test_fixture;
 
 pub use reentrancy_guard::ReentrancyGuard;
 pub use state_machine::StateMachine;
+
+#[cfg(any(test, feature = "testutils"))]
+pub use test_fixture::{MockKYCRegistry, MockMNT, MockSanctions, MockSnapshot};
